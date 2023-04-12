8f78d7947ff442677072a00a9dc7f141e8920487
Ben De Meurichy <ben.demeurichy@gmail.com>
2023-04-12 18:22:41 +0200

    added 2 state switch

--- app/src/main/java/com/ugnet/sel1/ui/components/SwitchButton2.kt
+++ app/src/main/java/com/ugnet/sel1/ui/components/SwitchButton2.kt
@@ -1,6 +1,12 @@
 package com.ugnet.sel1.ui.components
 
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.AnimationConstants.DefaultDurationMillis
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
@@ -8,9 +14,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -31,8 +35,8 @@ fun SwitchButton2(
 
     Row(
         modifier = modifier
-            .clip(RoundedCornerShape(20.dp))
-            .background(MainGroen)
+            .clip(RoundedCornerShape(30.dp))
+            .background(AccentLicht)
             .clickable {
                 setCurrentState(!currentState)
                 onStateChanged(!currentState)
@@ -60,34 +64,73 @@ fun ToggleButton(
     onOptionSelected: (String) -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    Row(
-        modifier = modifier
+    Box(
+        modifier = Modifier
+            .height(55.dp)
+            .width(180.dp) // set a fixed width for the box
+            .clip(RoundedCornerShape(30.dp))
             .background(Color.Transparent)
             .border(
-                width = 2.dp,
-                color = MainGroen,
-                shape = RoundedCornerShape(20.dp)
+                width = 1.dp,
+                color = Color.Gray,
+                shape = RoundedCornerShape(30.dp)
+
             )
             .padding(4.dp)
     ) {
-        options.forEach { option ->
-            val isSelected = option == selectedOption
-
-            Box(
-                modifier = Modifier
-                    .weight(1f)
-                    .clip(RoundedCornerShape(20.dp))
-                    .background(if (isSelected) AccentLicht else Color.Transparent)
-                    .clickable { onOptionSelected(option) },
-                contentAlignment = Alignment.Center
-            ) {
-                Text(
-                    modifier = Modifier.padding(8.dp),
-                    text = option,
-                    style = MaterialTheme.typography.body1,
-                    textAlign = TextAlign.Center,
-                    color = if (isSelected) Color.Black else Color.White,
+        Row(
+            modifier = modifier
+                .background(Color.Transparent)
+                .border(
+                    width = 2.dp,
+                    color = AccentLicht,
+                    shape = RoundedCornerShape(20.dp)
                 )
+                .padding(4.dp)
+        ) {
+            options.forEach { option ->
+                val isSelected = option == selectedOption
+                val visible by remember { mutableStateOf(true) }
+                val color by animateColorAsState(
+                    targetValue = if (isSelected) MainGroen else Color.Transparent,
+                    animationSpec = tween(
+                        durationMillis = DefaultDurationMillis,
+                        delayMillis = 0
+                    )
+
+                )
+                val textcolor by animateColorAsState(
+                    targetValue = if (isSelected) Color.White else Color.Black,
+                    animationSpec = tween(
+                        durationMillis = DefaultDurationMillis,
+                        delayMillis = 0
+                    )
+
+                )
+
+                AnimatedVisibility(visible = visible, enter = fadeIn(), exit = fadeOut()
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .weight(1f)
+                            .clip(RoundedCornerShape(20.dp))
+                            .background(
+                                color = color,
+                                shape = RoundedCornerShape(20.dp)
+                            )
+                            .clickable { onOptionSelected(option) },
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(
+                            modifier = Modifier.padding(8.dp),
+                            text = option,
+                            style = MaterialTheme.typography.body1,
+                            textAlign = TextAlign.Center,
+                            color = textcolor,
+                            softWrap = true
+                        )
+                    }
+                }
             }
         }
     }

3d764cce5c4f19e669e2817c5114a00074ed1956
Ben De Meurichy <ben.demeurichy@gmail.com>
2023-04-13 20:34:53 +0200

    added issue card

--- app/src/main/java/com/ugnet/sel1/ui/components/IssueCard.kt
+++ app/src/main/java/com/ugnet/sel1/ui/components/IssueCard.kt
@@ -2,12 +2,15 @@ package com.ugnet.sel1.ui.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Info
+import androidx.compose.material.icons.rounded.LocationOn
 import androidx.compose.material.icons.rounded.Person
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -16,6 +19,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.ugnet.sel1.ui.theme.AccentLicht
 import com.ugnet.sel1.ui.theme.MainGroen
 
@@ -30,26 +34,81 @@ fun IssueCard(title: String,
         .background(Color.Transparent)
         .clip(RoundedCornerShape(10.dp))
         .wrapContentWidth(), contentColor = Color.Transparent) {
+            //everythingcontainer
             Row(modifier = Modifier
                 .background(MainGroen)
                 .clip(RoundedCornerShape(10.dp))
                 .wrapContentWidth()
-                .height(60.dp)) {
+                .height(90.dp)) {
                 Column(modifier = Modifier
+                    .width(150.dp)
                     .padding(5.dp)
-                    .width(120.dp)) {
+                    ) {
                     //left side
+                    //title
                     Text(text = title, color = AccentLicht, modifier = Modifier.padding(1.dp))
-                    Row(modifier = Modifier
+                    //name
+                    Row(verticalAlignment = Alignment.CenterVertically,modifier = Modifier
                         .clip(RoundedCornerShape(30.dp))
                         .background(AccentLicht)) {
-                        Icon(imageVector = Icons.Rounded.Person, contentDescription = "person", tint = Color.Black, modifier = Modifier.padding(2.dp))
+                        Icon(imageVector = Icons.Rounded.Person, contentDescription = "person", tint = Color.Black, modifier = Modifier
+                            .padding(2.dp)
+                            .size(10.dp))
                         Text(
                             text = tennant,
                             color = Color.Black,
-                            style = MaterialTheme.typography.overline,
+                            style = MaterialTheme.typography.body1,
+                            fontSize = 10.sp,
                             modifier = Modifier.padding(2.dp)
                         )
+                    }
+                    //lowest container
+                    Row(modifier= Modifier
+                        .padding(0.dp, 8.dp, 0.dp, 8.dp)
+                        .width(150.dp),horizontalArrangement = Arrangement.Start) {
+                        //room
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically, modifier = Modifier
+                                .clip(RoundedCornerShape(30.dp))
+                                .background(AccentLicht)
+                        ) {
+                            Icon(
+                                imageVector = Icons.Rounded.LocationOn,
+                                contentDescription = "person",
+                                tint = Color.Black,
+                                modifier = Modifier
+                                    .padding(2.dp)
+                                    .size(10.dp)
+                            )
+                            Text(
+                                text = room,
+                                color = Color.Black,
+                                style = MaterialTheme.typography.body1,
+                                fontSize = 10.sp,
+                                modifier = Modifier.padding(2.dp)
+                            )
+                        }
+                        Spacer(modifier =Modifier.width(5.dp))
+                        //info
+                        Row (verticalAlignment = Alignment.CenterVertically, modifier = Modifier
+                            .clip(RoundedCornerShape(30.dp))
+                            .background(AccentLicht)) {
+                        Icon(
+                            imageVector = Icons.Rounded.Info,
+                            contentDescription = "person",
+                            tint = Color.Black,
+                            modifier = Modifier
+                                .padding(2.dp)
+                                .size(10.dp)
+                        )
+                        Text(
+                            text = description.value,
+                            color = Color.Black,
+                            style = MaterialTheme.typography.body1,
+                            fontSize = 10.sp,
+                            modifier = Modifier.padding(2.dp)
+                        )
+                    }
                     }
                 }
                 Column(modifier = Modifier

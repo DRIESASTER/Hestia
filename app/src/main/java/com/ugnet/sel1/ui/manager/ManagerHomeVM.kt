c9b87b7215e51b6dba4ab45d3f665f3aa6c78064
Ben De Meurichy <ben.demeurichy@gmail.com>
2023-04-15 23:03:22 +0200

    added main screen for managerside

--- app/src/main/java/com/ugnet/sel1/ui/manager/ManagerHomeVM.kt
+++ app/src/main/java/com/ugnet/sel1/ui/manager/ManagerHomeVM.kt
@@ -5,7 +5,11 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.ugnet.sel1.domain.repository.IssueResponse
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.ugnet.sel1.domain.models.Response
+import com.ugnet.sel1.domain.repository.IssuesResponse
+import com.ugnet.sel1.domain.repository.PandenResponse
 import com.ugnet.sel1.domain.useCases.UseCases
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -15,27 +19,34 @@ import javax.inject.Inject
 //add issues later
 @HiltViewModel
 class ManagerHomeVM @Inject constructor(private val useCases:UseCases) : ViewModel() {
+
+
     //TODO: fix with issuedata
-    var issues by mutableStateOf<List<IssueResponse>>(emptyList())
+    var IssueResponse by mutableStateOf<IssuesResponse>(Response.Loading)
         private set
 
-
-    var properties by mutableStateOf<List<PropertyData>>(emptyList())
+    var PropertyResponse by mutableStateOf<PandenResponse>(Response.Loading)
         private set
 
-    var selectedView by mutableStateOf(true)
-
-    //FIXME: add user id
-    private fun getIssues() = viewModelScope.launch {
-        useCases.getIssue("help").collect { response ->
-            issues = listOf(response)
-        }
+    init{
+        //getIssues()
+        //getProperties()
     }
+
+    val uid: String? = Firebase.auth.currentUser?.uid
+    //FIXME: add user id
+//    private fun getIssues() = viewModelScope.launch {
+//        if(uid != null){
+//            useCases.getIssues(uid).collect { response ->
+//                IssueResponse = listOf<>(response)
+//            }
+//        }
+//    }
 
     //FIXME: add user id
 //    private fun getProperties() = viewModelScope.launch {
-//        useCases.getOwnedPanden().collect { response:PropertyData ->
-//            properties = listOf(response)
+//        useCases.getOwnedPanden(uid).collect { response ->
+//            PropertyResponse = response
 //        }
 //    }
 

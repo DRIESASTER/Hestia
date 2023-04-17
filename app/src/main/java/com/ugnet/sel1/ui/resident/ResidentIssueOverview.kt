5171ea611ccefc73aeba2380764179a30438bfad
Thibaud Collyn <thibaudcollyn@gmail.com>
2023-04-17 23:06:19 +0200

    Add issue button added

--- app/src/main/java/com/ugnet/sel1/ui/resident/ResidentIssueOverview.kt
+++ app/src/main/java/com/ugnet/sel1/ui/resident/ResidentIssueOverview.kt
@@ -1,33 +1,54 @@
 package com.ugnet.sel1.ui.resident
 
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.ugnet.sel1.domain.models.Status
 import com.ugnet.sel1.ui.manager.IssueData
+import com.ugnet.sel1.ui.theme.MainGroen
 
 @Composable
 fun ResidentIssueOverview(
     modifier: Modifier = Modifier,
     issues:List<IssueData>,
 ) {
-    Surface(modifier = modifier) {
-        Row(
-            modifier = modifier
+    Surface(
+        modifier = modifier,
+
+    ) {
+        Column(
+            modifier = modifier.fillMaxWidth().fillMaxHeight(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
             if (issues.isEmpty()) {
-                Text(
-                    text = "you currently have no issues",
-                    color = Color.Gray.copy(alpha = 0.5f),
-                )
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth(1.0f)
+                        //.fillMaxSize(1.0f) // it will fill parent box
+                        .padding(8.dp)
+                ) {
+                    Text(
+                        text = "you currently have no issues",
+                        color = Color.Gray.copy(alpha = 0.5f),
+                        fontSize = 24.sp,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier.align(Alignment.Center)
+                    )
+                }
             } else {
                 LazyColumn(
                     horizontalAlignment = Alignment.CenterHorizontally,
@@ -44,7 +65,26 @@ fun ResidentIssueOverview(
                     }
                 }
             }
-            //TODO: add addIssue button
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth(1.0f)
+                    .padding(8.dp),
+            ) {
+                OutlinedButton(
+                    onClick = { /*TODO: make click add direct to add issue screen*/ },
+                    modifier = Modifier.size(50.dp).align(Alignment.BottomCenter),
+                    shape = CircleShape,
+                    border = BorderStroke(5.dp, MainGroen),
+                    contentPadding = PaddingValues(0.dp),
+                    colors = ButtonDefaults.outlinedButtonColors(contentColor = Color.Blue)
+                ) {
+                    Icon(
+                        imageVector = Icons.Rounded.Add,
+                        contentDescription = "add issue button",
+                        tint = MainGroen
+                    )
+                }
+            }
         }
     }
 }
@@ -52,7 +92,7 @@ fun ResidentIssueOverview(
 @Preview
 @Composable
 fun ResidentIssueOverviewPreview() {
-    ResidentIssueOverview(issues = createMockIssueDataList())
+    ResidentIssueOverview(issues = emptyList())
 }
 
 fun createMockIssueDataList(): List<IssueData> {

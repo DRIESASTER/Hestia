f1c3497a77d008ba49dbb17fb698a404343bc43d
Ben De Meurichy <ben.demeurichy@gmail.com>
2023-05-15 15:39:17 +0200

    announcement popup

--- app/src/main/java/com/ugnet/sel1/ui/announcements/AnnouncementsScreen.kt
+++ app/src/main/java/com/ugnet/sel1/ui/announcements/AnnouncementsScreen.kt
@@ -1,8 +1,6 @@
 package com.ugnet.sel1.ui.announcements
 
-import android.util.Log
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
@@ -10,20 +8,14 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.*
-
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.ugnet.sel1.common.snackbar.SnackbarManager
 import com.ugnet.sel1.domain.models.Announcement
-import com.ugnet.sel1.domain.models.Response
 import com.ugnet.sel1.navigation.MyDestinations
 import com.ugnet.sel1.ui.components.DrawerBody
 import com.ugnet.sel1.ui.components.DrawerHeader
@@ -39,30 +31,7 @@ import kotlinx.coroutines.launch
 fun AnnouncementScreen(viewModel: AnnouncementsViewModel, navigate: (String) -> Unit) {
     val scaffoldState = rememberScaffoldState()
     val coroutineScope = rememberCoroutineScope()
-
-    //hamburgermenu
-    val drawerItems = listOf(
-        MenuItem(
-            name = "Home",
-            route = MyDestinations.MANAGER_HOME_ROUTE,
-            icon = Icons.Rounded.Home,
-        ),
-        MenuItem(name= "Logout",
-            route = "logout",
-            icon = Icons.Rounded.ExitToApp),
-
-        MenuItem(
-            name = "Profile",
-            route = MyDestinations.PROFILE_ROUTE,
-            icon = Icons.Rounded.Person,
-        ),
-        MenuItem(
-            name = "Announcements",
-            route = MyDestinations.ANNOUNCEMENT_ROUTE,
-            icon = Icons.Rounded.Campaign,
-        )
-    )
-
+    var isPopupVisible by remember { mutableStateOf(false) }
     when (val uiState = viewModel.uiState.collectAsState().value) {
         AnnouncementUiState.Loading -> {
             // Show loading state UI
@@ -70,6 +39,34 @@ fun AnnouncementScreen(viewModel: AnnouncementsViewModel, navigate: (String) -> 
         is AnnouncementUiState.Success -> {
             val user = uiState.currentUser
             val announcements = uiState.announcements
+
+            //hamburgermenu
+            var homeroute = MyDestinations.MANAGER_HOME_ROUTE
+            if (user.accountType=="Huurder"){
+                homeroute = MyDestinations.HIREE_HOME_ROUTE
+            }
+
+            val drawerItems = listOf(
+                MenuItem(
+                    name = "Home",
+                    route = homeroute,
+                    icon = Icons.Rounded.Home,
+                ),
+                MenuItem(name= "Logout",
+                    route = "logout",
+                    icon = Icons.Rounded.ExitToApp),
+
+                MenuItem(
+                    name = "Profile",
+                    route = MyDestinations.PROFILE_ROUTE,
+                    icon = Icons.Rounded.Person,
+                ),
+                MenuItem(
+                    name = "Announcements",
+                    route = MyDestinations.ANNOUNCEMENT_ROUTE,
+                    icon = Icons.Rounded.Campaign,
+                )
+            )
 
             Scaffold(
                 modifier = Modifier.fillMaxSize(1.0f),
@@ -87,7 +84,7 @@ fun AnnouncementScreen(viewModel: AnnouncementsViewModel, navigate: (String) -> 
                 floatingActionButton = {
                     if (user.accountType == "Manager") {
                         addButton(contentDescription = "Add property", onClick = {
-                            navigate(MyDestinations.ANNOUNCEMENT_ADD_ROUTE)
+                            isPopupVisible = true
                         })
                     }
                 },
@@ -103,7 +100,13 @@ fun AnnouncementScreen(viewModel: AnnouncementsViewModel, navigate: (String) -> 
                             announcements = announcements
                         )
                     }
-                }
+                    if(isPopupVisible) {
+                        AlertDialog(
+                            modifier=Modifier.fillMaxWidth().wrapContentHeight(),
+                            onDismissRequest = { isPopupVisible = false },
+                            text = { AnnouncementsAddScreen() }, buttons = {})
+                    }
+                    }
             )
         }
     }
@@ -116,11 +119,18 @@ fun AnnouncementOverview(modifier: Modifier = Modifier, announcements:List<Annou
             .fillMaxWidth()
             .fillMaxHeight(0.9f)){
             itemsIndexed(announcements) { _, announcement ->
-                Card(backgroundColor = MainGroen, modifier = Modifier.fillMaxWidth().padding(20.dp).clip(
-                        RoundedCornerShape(20.dp)),
+                Card(backgroundColor = MainGroen, modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(20.dp)
+                    .clip(
+                        RoundedCornerShape(20.dp)
+                    ),
                     elevation = 8.dp
                     ) {
-                    Column(modifier = Modifier.clip(RoundedCornerShape(30.dp)).padding(15.dp).background(AccentLicht, shape = RoundedCornerShape(20.dp))
+                    Column(modifier = Modifier
+                        .clip(RoundedCornerShape(30.dp))
+                        .padding(15.dp)
+                        .background(AccentLicht, shape = RoundedCornerShape(20.dp))
                     ) {
                         Icon(imageVector = Icons.Rounded.Notifications, contentDescription = "alert",tint= RedProgress,modifier = Modifier
                             .padding(4.dp)
